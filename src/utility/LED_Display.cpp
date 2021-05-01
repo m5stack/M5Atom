be137ee101670843ac91d3af8b4f596533810518
Xylopyrographer <xylopyrographer@gmail.com>
2021-05-01 14:45:23 -0600

    Add `fillipix(CRGB Color)` method
    Adding the `fillipix(CRGB Color)` method as described in the ATM API documents.
    Also corrects the spelling error in the provided LED libraries.

---
+++ src/utility/LED_Display.cpp
@@ -0,0 +1,166 @@
+#include "LED_Display.h"
+
+LED_Display::LED_Display(uint8_t LEDNumbre)
+{
+    FastLED.addLeds<WS2812, DATA_PIN>(_ledbuff, LEDNumbre);
+    _xSemaphore = xSemaphoreCreateMutex();
+    _numberled = LEDNumbre;
+}
+
+LED_Display::~LED_Display()
+{
+
+}
+
+void LED_Display::run(void *data)
+{
+    data = nullptr;
+
+    for (int num = 0; num <= 24; num++) // was: for (int num = 0; num < 26; num++)
+    {
+        _ledbuff[num] = 0x000000;
+    }
+    FastLED.show();
+    FastLED.setBrightness(20);
+
+    while (1)
+    {
+        xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+        if (_mode == kAnmiation_run)
+        {
+            if ((_am_mode & kMoveRight) || (_am_mode & kMoveLeft))
+            {
+                if (_am_mode & kMoveRight)
+                {
+                    _count_x++;
+                }
+                else
+                {
+                    _count_x--;
+                }
+            }
+            if ((_am_mode & kMoveTop) || (_am_mode & kMoveButtom))
+            {
+                if (_am_mode & kMoveTop)
+                {
+                    _count_y--;
+                }
+                else
+                {
+                    _count_y++;
+                }
+            }
+            if ((_am_count != -1) && (_am_count != 0))
+            {
+                _am_count--;
+                if (_am_count == 0)
+                {
+                    _mode = kAnmiation_stop;
+                }
+            }
+            displaybuff(_am_buffptr, _count_x, _count_y);
+            delay(_am_speed);
+            delay(10);
+            xSemaphoreGive(_xSemaphore);
+        }
+        else
+        {
+            delay(10);
+            xSemaphoreGive(_xSemaphore);
+        }
+        
+        FastLED.show();
+    }
+}
+
+void LED_Display::animation(uint8_t *buffptr, uint8_t amspeed, uint8_t ammode, int64_t amcount)
+{
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    if (_mode == kAnmiation_run)
+    {
+        _mode = kAnmiation_stop;
+    }
+    _am_buffptr = buffptr;
+    _am_speed = amspeed;
+    _am_mode = ammode;
+    _am_count = amcount;
+    _count_x = _count_y = 0;
+    _mode = kAnmiation_run;
+    xSemaphoreGive(_xSemaphore);
+}
+
+void LED_Display::displaybuff(uint8_t *buffptr, int8_t offsetx, int8_t offsety)
+{
+    uint16_t xsize = 0, ysize = 0;
+    xsize = buffptr[0];
+    ysize = buffptr[1];
+
+    offsetx = offsetx % xsize;
+    offsety = offsety % ysize;
+
+    int8_t setdatax = (offsetx < 0) ? (-offsetx) : (xsize - offsetx);
+    int8_t setdatay = (offsety < 0) ? (-offsety) : (ysize - offsety);
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    for (int x = 0; x < 5; x++)
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            _ledbuff[x + y * 5].raw[1] = buffptr[2 + ((setdatax + x) % xsize + ((setdatay + y) % ysize) * xsize) * 3 + 0];
+            _ledbuff[x + y * 5].raw[0] = buffptr[2 + ((setdatax + x) % xsize + ((setdatay + y) % ysize) * xsize) * 3 + 1];
+            _ledbuff[x + y * 5].raw[2] = buffptr[2 + ((setdatax + x) % xsize + ((setdatay + y) % ysize) * xsize) * 3 + 2];
+        }
+    }
+    xSemaphoreGive(_xSemaphore);
+    FastLED.setBrightness(20);
+}
+
+void LED_Display::setBrightness(uint8_t brightness)
+{
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    FastLED.setBrightness(brightness);
+    xSemaphoreGive(_xSemaphore);
+}
+
+void LED_Display::drawpix(uint8_t xpos, uint8_t ypos, CRGB Color)
+{
+    if ((xpos >= 5) || (ypos >= 5))
+    {
+        return;
+    }
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    _ledbuff[xpos + ypos * 5] = Color;
+    xSemaphoreGive(_xSemaphore);
+}
+
+void LED_Display::drawpix(uint8_t Number, CRGB Color)
+{
+    if (Number >= 25)
+    {
+        return;
+    }
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    _ledbuff[Number] = Color;
+    xSemaphoreGive(_xSemaphore);
+}
+
+// modified 2021-01-29 by Xylopyrographer to include this fillpix() function
+void LED_Display::fillpix(CRGB Color)
+{
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    for (int8_t i = 0; i <= 24; i++)
+    {
+        _ledbuff[i] = Color;
+    }
+    xSemaphoreGive(_xSemaphore);
+}
+
+
+void LED_Display::clear()
+{
+    xSemaphoreTake(_xSemaphore, portMAX_DELAY);
+    for (int8_t i = 0; i <= 24; i++)
+    {
+        _ledbuff[i] = 0;
+    }
+    xSemaphoreGive(_xSemaphore);
+}
---
+++ src/utility/LED_Display.h
@@ -0,0 +1,59 @@
+#ifndef _LED_DISPLAY_H_
+#define _LED_DISPLAY_H_
+
+#include <FastLED.h>
+#include "utility/Task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define NUM_LEDS 25
+#define DATA_PIN 27
+
+class LED_Display : public Task
+{
+private:
+    CRGB _ledbuff[NUM_LEDS];
+    uint8_t _numberled;
+
+    uint8_t _mode;
+    uint16_t _am_speed;
+    uint8_t _am_mode;
+    int32_t _count_x, _count_y;
+    int32_t _am_count = -1;
+    uint8_t *_am_buffptr;
+
+    SemaphoreHandle_t _xSemaphore = NULL;
+
+public:
+    enum
+    {
+        kStatic = 0,
+        kAnmiation_run,
+        kAnmiation_stop,
+    } Dismode;
+    enum
+    {
+        kMoveRight = 0x01,
+        kMoveLeft = 0x02,
+        kMoveTop = 0x04,
+        kMoveButtom = 0x08,
+    } Am_mode;
+
+    /* data */
+public:
+    LED_Display(uint8_t LEDNumbre = 25);
+    ~LED_Display();
+    void run(void *data);
+
+    void animation(uint8_t *buffptr, uint8_t amspeed, uint8_t ammode, int64_t amcount = -1);
+    void displaybuff(uint8_t *buffptr, int8_t offsetx = 0, int8_t offsety = 0);
+    void MoveDisPlayBuff(int8_t offsetx = 0, int8_t offsety = 0);
+
+    void setBrightness(uint8_t brightness);
+    void drawpix(uint8_t xpos, uint8_t ypos, CRGB Color);
+    void drawpix(uint8_t Number, CRGB Color);
+    void fillpix(CRGB Color);   // this line added 2021-01-29 by Xylopyrographer
+    void clear();
+};
+
+#endif
