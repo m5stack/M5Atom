5f3c2d134f84dbee29a997400cdbcac9f1b57307
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-06 12:26:34 +0800

    some

--- examples/Advanced/Serial/MultSerial/MultSerial.ino
+++ examples/Advanced/Serial/MultSerial/MultSerial.ino
@@ -1,19 +1,19 @@
 /*
 *******************************************************************************
 * Copyright (c) 2021 by M5Stack
-*                  Equipped with M5Core2 sample source code
-*                          配套  M5Core2 示例源代码
-* Visit the website for more information：https://docs.m5stack.com/en/core/core2
-* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/core2
+*                  Equipped with Atom-Lite/Matrix sample source code
+*                          配套  Atom-Lite/Matrix 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/products
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/products
 *
 * describe：MultSerial.  多串口
 * date：2021/8/5
 ******************************************************************************
 */
-#include <M5Core2.h>
+#include <M5Atom.h>
 
 void setup() {
-  M5.begin(); //Init M5Core2.  初始化 M5Core2
+  M5.begin(); //Init M5Atom.  初始化 M5Atom
   // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
   Serial2.begin(115200, SERIAL_8N1, 16, 17);  //Init serial port 2.  初始化串口2
 }
