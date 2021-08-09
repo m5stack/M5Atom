9aa17d1f2a2c3d378fb677c262b52d23e36fa6f1
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-09 16:39:51 +0800

    angle

--- examples/Unit/ANGLE/ANGLE.ino
+++ examples/Unit/ANGLE/ANGLE.ino
@@ -1,46 +1,36 @@
 /*
 *******************************************************************************
 * Copyright (c) 2021 by M5Stack
-*                  Equipped with M5Stick-C sample source code
-*                          配套  M5Stick-C 示例源代码
-* Visit the website for more information：https://docs.m5stack.com/en/core/m5stickc
-* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/m5stickc
+*                  Equipped with Atom-Lite/Matrix sample source code
+*                          配套  Atom-Lite/Matrix 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/products
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/products
 *
-* describe：WiFi connect.  wifi连接
-* date：2021/7/27
+* describe：Angle.  角度计
+* date：2021/8/9
 *******************************************************************************
+  Description: Read the Angle of the angometer and convert it to digital display
+  读取角度计的角度，并转换为数字量显示
 */
 
-/*
-    Description: Read ANGLE Unit input analog quantity, and convert to 12-bit digital quantity for display 
-*/
+#include <M5Atom.h>
+int sensorPin = 32; // set the input pin for the potentiometer.  设置角度计的输入引脚
 
-#include <M5Stack.h>
-// select the input pin for the potentiometer
-int sensorPin = 36;
-// last variable to store the value coming from the sensor
-int last_sensorValue = 100;
-// current variable to store the value coming from the sensor
-int cur_sensorValue = 0;
+int last_sensorValue = 100; // Stores the value last read by the sensor.  存储传感器上次读取到的值
+int cur_sensorValue = 0;  // Stores the value currently read by the sensor.  存储传感器当前读取到的值
 
 void setup() {
-  M5.begin();
-  M5.Power.begin();
-  pinMode(sensorPin, INPUT);
+  M5.begin(); //Init M5Atom.  初始化 M5Atom
+  pinMode(sensorPin, INPUT);  //Sets the specified pin to input mode.  设置指定引脚为输入模式
   dacWrite(25, 0);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("the value of ANGLE: ");
 }
 
 void loop() {
-  // read the value from the sensor:
-  cur_sensorValue = analogRead(sensorPin);
-  M5.Lcd.setCursor(0, 25);
-  if(abs(cur_sensorValue - last_sensorValue) > 10){//debaunce
-    M5.Lcd.fillRect(0, 25, 100, 25, BLACK);
-    M5.Lcd.print(cur_sensorValue);
+  cur_sensorValue = analogRead(sensorPin);  // read the value from the sensor.  读取当前传感器的值
+  if(abs(cur_sensorValue - last_sensorValue) > 10){ //If the difference is more than 10.  如果差值超过10
+    Serial.print("the value of ANGLE: ");
+    Serial.println(cur_sensorValue);
     last_sensorValue = cur_sensorValue;
   }
-  delay(50);
+  delay(200);
 }
