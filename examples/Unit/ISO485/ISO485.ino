425783bb40c76a3d0347db6a6593f31df18e7b48
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-30 14:32:58 +0800

    iso485

---
+++ examples/Unit/ISO485/ISO485.ino
@@ -0,0 +1,40 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with Atom-Lite/Matrix sample source code
+*                          配套  Atom-Lite/Matrix 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/unit/iso485
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/unit/iso485
+*
+* describe: iso485.
+* date：2021/8/30
+*******************************************************************************
+  Please connect to Port ,Pressed ButtonA :send "hello world"
+  请连接端口 ,Pressed ButtonA :send "hello world"
+*/
+
+#include <M5Atom.h>
+
+String str = "";
+
+void setup() {
+  M5.begin();
+  Serial.println("ISO485");
+  Serial2.begin(115200, SERIAL_8N1, 26, 32);
+}
+
+void loop() {
+  if (M5.Btn.wasPressed()){
+    Serial2.write("HELLO World\r\n");
+  }
+
+  if (Serial2.available()) {
+    char ch = Serial2.read();
+    str += ch;
+    if (str.endsWith("\r\n")) {
+      Serial.print(str);
+      str = "";
+    }
+  }
+  M5.update();
+}
